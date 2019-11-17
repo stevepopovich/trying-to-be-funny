bd2ac26772d8d11a9589a2bb4b3463e025e9c247
steven.popovich <steven.popovich@gc.com>
2019-11-16 22:37:02 -0500

    have a little fragment transition

--- app/src/main/java/com/stevenpopovich/trying_to_be_funny/ui/main/OnFinishRecordingFragment.kt
+++ app/src/main/java/com/stevenpopovich/trying_to_be_funny/ui/main/OnFinishRecordingFragment.kt
@@ -7,12 +7,39 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.stevenpopovich.trying_to_be_funny.R
 
+
 class OnFinishRecordingFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.do_you_want_to_save, container, false)
+        return inflater.inflate(R.layout.on_finished_recording_container, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val childFragment = WouldYouLikeToSaveFragment()
+        val transaction = childFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(
+            R.anim.enter_from_right,
+            R.anim.exit_to_left,
+            R.anim.enter_from_left,
+            R.anim.exit_to_right
+        )
+        transaction.replace(R.id.on_finished_recording_container, childFragment).commit()
+
+//        val transaction = fragmentManager!!.beginTransaction()
+//        transaction.setCustomAnimations(
+//            R.anim.enter_from_right,
+//            R.anim.exit_to_left,
+//            R.anim.enter_from_left,
+//            R.anim.exit_to_right
+//        )
+//        val addTagsFragment = AddTagsFragment()
+//        transaction.replace(R.id.on_finished_recording_container, addTagsFragment)
+//        transaction.addToBackStack(null)
+//        transaction.commit()
     }
 }
---
+++ app/src/main/java/com/stevenpopovich/trying_to_be_funny/ui/main/WouldYouLikeToSaveFragment.kt
@@ -0,0 +1,44 @@
+package com.stevenpopovich.trying_to_be_funny.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.stevenpopovich.trying_to_be_funny.R
+import kotlinx.android.synthetic.main.do_you_want_to_save.*
+
+class WouldYouLikeToSaveFragment : Fragment() {
+    private val nextFragment = AddTagsFragment()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.do_you_want_to_save, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        yes_I_want_to_save.setOnClickListener {
+            transitionToNextFragment()
+        }
+
+        no_I_dont_want_to_save.setOnClickListener {
+            transitionToNextFragment()
+        }
+    }
+
+    private fun transitionToNextFragment() {
+        val transaction = fragmentManager!!.beginTransaction()
+        transaction.setCustomAnimations(
+            R.anim.enter_from_right,
+            R.anim.exit_to_left,
+            R.anim.enter_from_left,
+            R.anim.exit_to_right
+        )
+        transaction.replace(R.id.on_finished_recording_container, nextFragment).commit()
+    }
+}
