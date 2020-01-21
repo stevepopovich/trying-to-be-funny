c729ff85bb7a8e190b0a5b86d03306382112464c
steven.popovich <steven.popovich@gc.com>
2020-01-20 23:37:18 -0500

    test all the trivial stuff in SetService

--- app/src/test/java/com/stevenpopovich/SetServiceTests.kt
+++ app/src/test/java/com/stevenpopovich/SetServiceTests.kt
@@ -1,5 +1,6 @@
 package com.stevenpopovich
 
+import com.stevenpopovich.trying_to_be_funny.SetDataInvalidError
 import com.stevenpopovich.trying_to_be_funny.SetService
 import com.stevenpopovich.trying_to_be_funny.SetServiceLocalSavingImpl
 import com.stevenpopovich.trying_to_be_funny.room.AppDatabase
@@ -30,5 +31,28 @@ class SetServiceTests {
         setService.deleteSet(idToDelete)
 
         verify(exactly = 1) { mockDatabase.setDao().deleteById(idToDelete) }
+    }
+
+    @Test
+    fun testGetAllBits() {
+        setService.getAllBits()
+
+        verify(exactly = 1) { mockDatabase.bitDao().getAll() }
+    }
+
+    @Test
+    fun testGetSet() {
+        val idToGet = UUID.randomUUID()
+
+        setService.getSet(idToGet)
+
+        verify(exactly = 1) { mockDatabase.setDao().get(idToGet) }
+    }
+
+    @Test(expected = SetDataInvalidError::class)
+    fun testSaveStaticSetThrowsWhenSetBitsIsNull() {
+        SetService.setBits = null
+
+        setService.saveStaticSet()
     }
 }
